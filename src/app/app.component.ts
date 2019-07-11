985ef29d850bbfa1c636a7e9d6424843fb66c059
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-07-11 19:06:33 -0300

    Aula03 validacoes e modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,8 @@
 import { Component } from '@angular/core';
-import { Email } from './models/email';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  private _isEmailOpen = false;
-
-  email: Email = new Email();
-
-  get isEmailOpen(){
-    return this._isEmailOpen;
-  }
- 
-  toggleEmailForm(){
-    this._isEmailOpen = !this.isEmailOpen
-  }
-
-  enviarEmail(eventoSubmit: Event){
-
-    eventoSubmit.preventDefault();
-    console.log(this.email);
-  }
-
-}
+export class AppComponent {}
