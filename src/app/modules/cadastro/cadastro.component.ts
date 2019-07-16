07e419d8a63df8c83b164198f791fbadfb7b23aa
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-07-15 22:55:18 -0300

    Aula 06 validacoes assincronas

--- src/app/modules/cadastro/cadastro.component.ts
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponseBase } from '@angular/common/http';
 import { UserInput } from 'src/app/models/dto/user-input';
 import { Router } from '@angular/router';
+import { map, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'cmail-cadastro',
@@ -20,7 +21,7 @@ export class CadastroComponent implements OnInit {
     nome: new FormControl('', Validators.required),
     username: new FormControl('', [Validators.required, Validators.minLength(2)]),
     senha: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    avatar: new FormControl('', Validators.required),
+    avatar: new FormControl('', Validators.required, this.validaImagem.bind(this)),
     telefone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{4}-?[0-9]{4}[0-9]?')]),
   })
 
@@ -29,7 +30,21 @@ export class CadastroComponent implements OnInit {
   constructor(private http: HttpClient
               ,private roteador: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  validaImagem(controle: FormControl){
+
+    return this.http
+                .head(controle.value,{observe: 'response'})
+                .pipe(
+                  map((resposta: HttpResponseBase) => {
+                    return true
+                  })
+                  ,catchError((httpError)=>{
+                    return [{urlInvalida: true}]
+                  })
+                )
+
   }
 
   cadastrar(){
@@ -49,8 +64,7 @@ export class CadastroComponent implements OnInit {
             this.roteador.navigate(['login', userApi.name])
           }
          ,erro => {
-           this.msgErro = 'Oops algo errado aconteceu tente mais tarde'
-           console.error(erro)
+           this.msgErro = `${erro.statusText}: Oops algo errado aconteceu tente mais tarde. ${erro.status}`
           }
         );
 
