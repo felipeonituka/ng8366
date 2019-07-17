1c2b8282e6963a04dc314e0ff24972c8c936f6a9
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-07-16 23:04:14 -0300

    Aula 07 LazyModules e Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CadastroModule } from './modules/cadastro/cadastro.module';
-import { CaixaDeEntradaModule } from './modules/caixa-de-entrada/caixa-de-entrada.module';
-import { LoginModule } from './modules/login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'cmail-login',
   templateUrl: './login.component.html',
-  styles: []
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  handleLogin(formLogin: NgForm){
+
+    if(formLogin.invalid){
+      formLogin.controls['email'].markAsTouched();
+      formLogin.controls['senha'].markAsTouched();
+      return
+    }
+
+    this.http
+        .post('http://localhost:3200/login',this.login)
+        .subscribe(
+          (res: any) => {
+            console.log(res);
+            localStorage.setItem('cmail-token', res.token)
+          }
+          , erro => {
+            console.log(erro);
+
+          }
+        );
+
+
+
   }
 
 }
